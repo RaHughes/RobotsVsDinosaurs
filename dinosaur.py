6f92366f718d5a637a43701402fbe883f14027d1
RaHughes <rogerallenhughes@hotmail.com>
2021-10-12 14:52:41 -0500

    Modified method to accurately output desired

--- dinosaur.py
+++ dinosaur.py
@@ -1,7 +1,12 @@
 class Dinosaur:
     def __init__(self, name, attack_power):
         self.name = name
+        self.health = 100
         self.attack_power = attack_power
 
     def attack(self, robot):
-        print('')    
+        print()
+        print(f'{self.name} attacks {robot.name} with a powerful bite for {self.attack_power}!')
+        robot.health -= self.attack_power    
+        print()
+        print(f'{robot.name} only has {robot.health} health left!')
