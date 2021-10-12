0ba19eeddfc8dcaa40fa8220432e466b3fdd06f3
RaHughes <rogerallenhughes@hotmail.com>
2021-10-12 12:34:26 -0500

    Added functionality for create_fleet method

--- fleet.py
+++ fleet.py
@@ -5,4 +5,9 @@ class Fleet:
         self.robots = []
 
     def create_fleet(self):
-        print('')
+        robot_one = Robot('Megatron')
+        robot_two = Robot('Optimus Prime')
+        robot_three = Robot('Bumble Bee')
+        self.robots.append(robot_one, robot_two, robot_three)
+        for robot in self.robots:
+            print(robot.name)
