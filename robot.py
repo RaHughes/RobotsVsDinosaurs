97e5bea52182b7c97ee09442a5d37d44d404bb89
RaHughes <rogerallenhughes@hotmail.com>
2021-10-12 14:54:22 -0500

    modified output for desired

--- robot.py
+++ robot.py
@@ -7,4 +7,8 @@ class Robot:
         self.weapon = Weapon('Sword', 30)
 
     def attack(self, dinosaur):
-        print('')
+        print()
+        print(f'{self.name} attacks {dinosaur.name} with a {self.weapon.name} for {self.weapon.attack_power}!')
+        dinosaur.health -= self.weapon.attack_power
+        print()
+        print(f'{dinosaur.name} only has {dinosaur.health} health left!')
