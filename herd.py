d781d78cac6de1d25dfc71f297ab404e24518a01
RaHughes <rogerallenhughes@hotmail.com>
2021-10-12 14:53:51 -0500

    Add functionality for all methods, modified for desired output

--- herd.py
+++ herd.py
@@ -5,9 +5,6 @@ class Herd:
         self.dinosaurs = []
 
     def create_herd(self):
-        dino_one = Dinosaur('T-Rex', 40)
-        dino_two = Dinosaur('Stego', 20)
-        dino_three = Dinosaur('Raptor', 10) 
-        self.dinosaurs.append(dino_one, dino_two, dino_three)
-        for dino in self.dinosaurs:
-            print(dino.name)       
+        self.dinosaurs.append(Dinosaur('T-Rex', 40))
+        self.dinosaurs.append(Dinosaur('Stego', 20))
+        self.dinosaurs.append(Dinosaur('Raptor', 10))     
