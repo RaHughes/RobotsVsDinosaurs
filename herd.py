306f3b164e8dc5b442f48be86decc84ed06b4983
RaHughes <rogerallenhughes@hotmail.com>
2021-10-12 12:49:46 -0500

    Added functionality for create_herd method

--- herd.py
+++ herd.py
@@ -5,4 +5,9 @@ class Herd:
         self.dinosaurs = []
 
     def create_herd(self):
-        print('')        
+        dino_one = Dinosaur('T-Rex', 40)
+        dino_two = Dinosaur('Stego', 20)
+        dino_three = Dinosaur('Raptor', 10) 
+        self.dinosaurs.append(dino_one, dino_two, dino_three)
+        for dino in self.dinosaurs:
+            print(dino.name)       
