d61da747667a83fa8433b8007265204faf30872d
RaHughes <rogerallenhughes@hotmail.com>
2021-10-13 10:08:14 -0500

    Modified main to only run one instance of battlefield

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
 from battlefield import Battlefield
 
+
 battlefield = Battlefield()
-
-battlefield.run_game()
-print()
-print('Thanks for playing!')
-print()
-print('would you like to start a new game?')
-
-user_input = input('Yes or No: ')
-
-if user_input == 'Yes':
-    battlefield.run_game()
-
-elif user_input == 'No':
-    print('Goodbye!')
+battlefield.run_game()      
